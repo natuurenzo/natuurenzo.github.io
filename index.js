533a206d445641f29d969c89e21eb20f5a569b19
sennrdesign <sennadeturner@gmail.com>
2016-10-06 17:16:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function dbcreate(){
+var db;
 
-}
+$('#button1').click(function (){
+  
+  db = openDatabase("Motorola", "1.0", "Moto Mobile Database", "200000");
+  
+  alert("database was created");
+  
+});
