e06c909d4eb17c802e2a8a76cae22cb517fa3640
sennrdesign <sennadeturner@gmail.com>
2016-10-06 18:51:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
-var country = document.getElementById("country");
-localStorage.setItem("Firstname",fname.value);
-localStorage.setItem("Lastname",lname.value);
-localStorage.setItem("Country",country.value);
 
-for(var i = 0; i < localStorage.key(); i++){
-  document.write('<table class="table table-bordered table-striped table-hover table-condensed"><thead><tr><th>'+localStorage.key(1)+"</th><th>"+localStorage.key(2)+"</th>"+localStorage.key(0)+"</th></thead>");
-}
