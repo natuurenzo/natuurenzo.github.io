40184324944f1ff6dab7780c2bf4c679cc6da299
sennrdesign <sennadeturner@gmail.com>
2016-10-08 13:11:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 $("document").ready(function() {
    $("#imgprops").addClass("img img-rounded img-responsive");
-   alert("Here I am!"); //This alert will go when I figure this out, I just added it to see if it pops up (it does).
 });
