94f867cf606603fa962451ae5a7522e69bba04ba
sennrdesign <sennadeturner@gmail.com>
2016-10-08 14:57:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-$("document").ready(function() {
-   $("#imgprops").addClass("img img-rounded img-responsive");
-});
+var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
+Array.prototype.memory_tile_shuffle = function(){
+   var i = this.length, j, temp;
+   while(--i > 0){
+      j = Math.floor(Math.random() * (i+1));
+      temp = this[j];
+      this[j] = this[i];
+      this[i] = temp;
+   }
+}
+function NewBoard(){
+ tiles_flipped = 0;
+ var output = '';
+ memory_array.memory_tile_shuffle();
+ for(var i = 0; i > memory_array.length; i++){
+    output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>'
+ }
+ document.getElementById("memory_board").innerHTML = output;
+}
